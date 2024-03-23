79d21377ce20eecb40fded75ff8ead9f17512b7f
Victor Mwenda <vmwenda.vm@gmail.com>
2024-03-23 20:49:25 +0300

    MsSQL - Administration. Base Tables

--- config/database/mssql.sql
+++ config/database/mssql.sql
@@ -2,8 +2,56 @@ create database dbcrudgen;
 
 use dbcrudgen;
 
-create table test
+create table packages
 (
-    id     INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
-    module varchar(255)
+    id   INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+    name text,
+    path text
 );
+
+create index index_packages on packages (id);
+create unique index unique_index_packages on packages (id);
+
+create table classes
+(
+    id         INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+    name       text,
+    package_id int foreign key references packages (id)
+);
+
+create index index_classes on classes (id);
+create unique index unique_index_classes on classes (id);
+
+create table class_methods
+(
+    id          INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+    name        text,
+    return_type text,
+    class_id    int foreign key references classes (id)
+);
+
+create index index_class_methods on class_methods (id);
+create unique index unique_index_class_methods on class_methods (id);
+
+create table class_variables
+(
+    id        INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+    name      text,
+    data_type text,
+    class_id  int foreign key references classes (id)
+);
+
+create index index_class_variables on class_variables (id);
+create unique index unique_index_class_variables on class_variables (id);
+
+create table method_variables
+(
+    id             INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+    name           text,
+    data_type      text,
+    method_id      int foreign key references class_methods (id),
+    variable_index int
+);
+
+create index index_method_variables on method_variables (id);
+create unique index unique_index_method_variables on method_variables (id);
